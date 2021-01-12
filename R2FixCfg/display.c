df1bc5c526c1901f52e2f4d3637c4617153c85c1
Spitfire_x86 <33195187+spitfirex86@users.noreply.github.com>
2021-01-12 14:46:16 +0100

    Fix duplicate resolutions

--- R2FixCfg/display.c
+++ R2FixCfg/display.c
@@ -17,35 +17,54 @@ BOOL IsSafeResolution( DISP_MODE *lpMode )
 	return FALSE;
 }
 
+BOOL IsDuplicateMode( DISP_MODE *lpMode, int nModes )
+{
+	// This is dumb, but apparently EnumDisplaySettings does not always return the resolutions
+	// in an order that makes sense. Which means we can't just compare it to the previous one
+	// to check if it's duplicated/already present in the array.
+
+	for ( int i = 0; i < nModes; i++ )
+	{
+		DISP_MODE *prevMode = &g_aDispModes[i];
+
+		if ( lpMode->dwWidth == prevMode->dwWidth && lpMode->dwHeight == prevMode->dwHeight )
+		{
+			return TRUE;
+		}
+	}
+
+	return FALSE;
+}
+
 void EnumResolutions( void )
 {
 	DEVMODE dm = { 0 };
 	dm.dmSize = sizeof(dm);
 
 	int idx = 0;
-	DISP_MODE lastMode = { 0 };
 
 	for ( int i = 0; EnumDisplaySettings(NULL, i, &dm) != 0; i++ )
 	{
-		// Skip display mode if resolution is the same as previous one
-		if ( dm.dmPelsWidth == lastMode.dwWidth && dm.dmPelsHeight == lastMode.dwHeight )
-			continue;
+		DISP_MODE mode = {
+			dm.dmPelsWidth,
+			dm.dmPelsHeight,
+			DMF_NONE
+		};
 
-		lastMode.dwWidth = dm.dmPelsWidth;
-		lastMode.dwHeight = dm.dmPelsHeight;
-		lastMode.dmfFlags = DMF_NONE;
+		if ( IsDuplicateMode(&mode, idx) )
+			continue;
 
 		DWORD ratio = 100 * dm.dmPelsHeight / dm.dmPelsWidth;
 
 		// Only add resolutions "close" to 4:3 or 5:4 ratio
 		if ( ratio > 73 && ratio < 82 )
 		{
-			if ( IsSafeResolution(&lastMode) )
+			if ( IsSafeResolution(&mode) )
 			{
-				lastMode.dmfFlags |= DMF_SAFE;
+				mode.dmfFlags |= DMF_SAFE;
 			}
 
-			g_aDispModes[idx++] = lastMode;
+			g_aDispModes[idx++] = mode;
 		}
 	}
 
